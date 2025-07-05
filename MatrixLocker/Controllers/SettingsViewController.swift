68454d09212984d13c50873b2f626a3da5db7dc1
Oleg Kizyma <oleg1203@gmail.com>
2025-07-05 06:21:23 +0300

    Refactor SettingsViewController to use individual view controllers for sidebar navigation and update Main.storyboard with new button outlets

--- MatrixLocker/Controllers/SettingsViewController.swift
+++ MatrixLocker/Controllers/SettingsViewController.swift
@@ -9,32 +9,55 @@ class SettingsViewController: NSViewController {
     // This will be the container for our different settings panes
     @IBOutlet weak var containerView: NSView!
     
-    private var tabViewController: NSTabViewController?
+    private var currentViewController: NSViewController?
     private var sidebarButtons: [NSButton] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        sidebarButtons = [generalButton, xcodeButton, aboutButton]
         
-        // Find the embedded TabViewController
-        for child in children {
-            if let vc = child as? NSTabViewController {
-                tabViewController = vc
-                break
-            }
-        }
+        // Set up button tags for identification
+        generalButton?.tag = 0
+        xcodeButton?.tag = 1
+        aboutButton?.tag = 2
         
-        // Set the initial state
+        sidebarButtons = [generalButton, xcodeButton, aboutButton].compactMap { $0 }
+        
+        // Set the initial state - show general settings
+        showViewController(withIdentifier: "GeneralSettingsViewController")
         updateSidebar(selectedButton: generalButton)
     }
     
     @IBAction func sidebarButtonTapped(_ sender: NSButton) {
-        // Switch tabs and update the UI
-        tabViewController?.selectedTabViewItemIndex = sender.tag
-        updateSidebar(selectedButton: sender)
+        let identifiers = ["GeneralSettingsViewController", "XcodeSettingsViewController", "AboutViewController"]
+        
+        if sender.tag < identifiers.count {
+            showViewController(withIdentifier: identifiers[sender.tag])
+            updateSidebar(selectedButton: sender)
+        }
     }
     
-    private func updateSidebar(selectedButton: NSButton) {
+    private func showViewController(withIdentifier identifier: String) {
+        // Remove current view controller if any
+        if let current = currentViewController {
+            current.view.removeFromSuperview()
+            current.removeFromParent()
+        }
+        
+        // Load new view controller
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        if let newViewController = storyboard.instantiateController(withIdentifier: identifier) as? NSViewController {
+            addChild(newViewController)
+            
+            // Add the new view to container
+            newViewController.view.frame = containerView.bounds
+            newViewController.view.autoresizingMask = [.width, .height]
+            containerView.addSubview(newViewController.view)
+            
+            currentViewController = newViewController
+        }
+    }
+    
+    private func updateSidebar(selectedButton: NSButton?) {
         // Visually update which button is currently selected
         for button in sidebarButtons {
             if button == selectedButton {
