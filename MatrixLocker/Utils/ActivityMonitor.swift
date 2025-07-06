6bab59bf495e3f451bed8815df074d03617007f9
Oleg Kizyma <oleg1203@gmail.com>
2025-07-06 03:42:58 +0300

    refactor: Remove unused UserSettings import from ActivityMonitor.swift

--- MatrixLocker/Utils/ActivityMonitor.swift
+++ MatrixLocker/Utils/ActivityMonitor.swift
@@ -1,6 +1,5 @@
 import Foundation
 import Cocoa
-import UserSettings
 
 // Your existing code below (assuming originalFile content starts here)
 // Since the original file content is not provided, this is a template including the requested imports.
